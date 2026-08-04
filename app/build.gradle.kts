65f17a24defbcb8b348862792f8a114df1497c39
TimHuang336699 <TimHuang336699@users.noreply.github.com>
2026-08-04 14:34:03 +0800

    Bump version to 0.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.tesla.dashboard"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.2.2"
+        versionCode = 7
+        versionName = "0.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
